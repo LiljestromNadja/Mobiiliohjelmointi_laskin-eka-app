c98a37ef32748864317f0e50996d5f8d458beb1d
Nadja Liljeström <zipieljege@gmail.com>
2023-08-25 15:57:08 +0300

    Ready

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [nro1, setNro1] = useState(0);
+  const [nro2, setNro2] = useState(0);
+
+  const [tulos, setTulos] = useState(nro1 + nro2);
+
+  const plussaa = () => {setTulos(nro1 + nro2)};
+  const miinusta = () => {setTulos(nro1-nro2)};
+
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+
+      <Text style={{marginBottom: 20}}>Result: {tulos}</Text>
+
+      <TextInput 
+      keyboardType='numeric' 
+      style={styles.input}
+      onChangeText={arvo1 => setNro1(Number(arvo1))}value={nro1} 
+      />
+
+      <TextInput 
+      keyboardType='numeric' 
+      style={styles.input}
+      onChangeText={arvo2 => setNro2(Number(arvo2))}value={nro2}       
+      />
+
+      <View style={{flexDirection: 'row', marginTop : 20 }}>
+
+        <Button onPress={plussaa} title=" + "></Button>
+        <Button onPress={miinusta} title=" - "></Button>
+
+      </View>
+
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +49,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width : 100,
+    borderWidth : 2,
+    borderColor : 'black',    
   },
 });
